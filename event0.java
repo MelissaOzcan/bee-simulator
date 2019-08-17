4e38baaf909abf07481692d5ffbd3ddf0e4a9f5b
Melissa Ozcan <34689581+MelissaOzcan@users.noreply.github.com>
2019-08-16 21:41:56 -0400

    Add files via upload

---
+++ event0.java
@@ -0,0 +1,39 @@
+package bee.event;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class event0 {
+    //TODO: please finish this event it was going so well
+    
+    /**
+     * in this event, the bee encounters a bear in its beehive.
+     * User has 5 seconds to decide if bee flies away or stays.
+     */
+    @SuppressWarnings("WeakerAccess")
+    public void event00() {
+        
+        // REFERENCE: https://stackoverflow.com/questions/2258066/java-run-a-function-after-a-specific-number-of-seconds
+        // When your program starts up
+        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+// then, when you want to schedule a task
+        /*
+        ~~the code that the lambda expression replaced~~
+        
+         Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Time is up!");
+            }
+        };
+         */
+        Runnable task = () -> System.out.println("Time is up!"); //this is a lambda expression?!?
+        executor.schedule(task, 5, TimeUnit.SECONDS);
+
+// and finally, when your program wants to exit
+        executor.shutdown();
+    }
+    
+}
