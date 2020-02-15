e53502c1ee2cb4f65ea66f430faf5fc969c79873
Melissa Ozcan <34689581+MelissaOzcan@users.noreply.github.com>
2020-02-15 15:44:55 -0500

    Add files via upload

--- generalFiles/BeeRunner.java
+++ generalFiles/BeeRunner.java
@@ -9,14 +9,12 @@ import bee.event.*;
 public class BeeRunner {
     
     public static void main(String[] args) {
-    
-        Random rand = new Random();
         Scanner in = new Scanner(System.in);
         Timer timer = new Timer();
         
         System.out.println("Welcome to the ~~BEE SIMULATOR~~ ");
         for (int i = 0; i < 20; i++)
-            System.out.print("\uD83D\uDC1D"); //emojis
+            System.out.print("\uD83D\uDC1D"); //these are bee emojis
         System.out.println();
     
         Bee bee = new Bee();
@@ -24,14 +22,12 @@ public class BeeRunner {
         
         System.out.println("\nYay! Your bee is all generated and ready to play!");
         
-        
-        
         int numOfRounds = 1; //keeps track of events played
         
         while (true) {
             
-            // user has option to quit every 3rd round
-            if (numOfRounds % 3 == 0) {
+            // user has option to quit every 4th round.
+            if (numOfRounds % 4 == 0) {
                 System.out.println("\nWould you like to keep playing? (Y/N)");
                 String input = in.nextLine();
                 switch (input) {
@@ -48,27 +44,9 @@ public class BeeRunner {
                         break;
                 }
             }
-    
-            new event1().event1(bee);
             
-            //decides which event will be played!
-            switch (rand.nextInt(5)) {
-                case 0:
-                    new event0();
-                    break;
-                case 1:
-                    new event1().event1(bee);
-                    break;
-                case 2:
-                    new event2();
-                    break;
-                case 3:
-                    new event3();
-                    break;
-                case 4:
-                    new event4();
-                    break;
-            }
+            eventPicker();
+            
             numOfRounds++;
         } //end of while(true) loop
         
@@ -76,5 +54,28 @@ public class BeeRunner {
         //update: ur a dumbie
     
         // 08/02/19 - you are the smartest person alive
+    }
+    
+    public static void eventPicker() {
+        Random rand = new Random();
+    
+        //decides which event will be played!
+        switch (rand.nextInt(5)) {
+            case 0:
+                new event0();
+                break;
+            case 1:
+                new event1();
+                break;
+            case 2:
+                new event2();
+                break;
+            case 3:
+                new event3();
+                break;
+            case 4:
+                new event4();
+                break;
+        }
     }
 }
